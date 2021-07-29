56825ef9d556ff80bf249017ac03343bfc478d46
curder <q.curder@gmail.com>
2021-07-29 14:32:57 +0800

    change title

--- store/posts.js
+++ store/posts.js
@@ -2,17 +2,17 @@ export const state = () => ({
    all: [
      {
        id: 'what-is-nuxt-js',
-       title: 'What is Nuxt JS',
+       title: 'What is Nuxt JS?',
        content: 'Build your next Vue.js application with confidence using NuxtJS. An open source framework making web development simple and powerful.',
      },
      {
        id: 'what-is-vue-js',
-       title: 'What is Vue JS',
+       title: 'What is Vue JS?',
        content: 'The Progressive JavaScript Framework',
      },
      {
        id: 'what-is-typescript',
-       title: 'What is TypeScript',
+       title: 'What is TypeScript?',
        content: 'TypeScript extends JavaScript by adding types. <br /> By understanding JavaScript, TypeScript saves you time catching errors and providing fixes before you run code. <br />Any browser, any OS, anywhere JavaScript runs. Entirely Open Source.',
      },
    ]
